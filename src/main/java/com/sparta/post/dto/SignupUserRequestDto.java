3214fa39184951a518b9741db464a59b40759810
haechanlee96 <131975479+haechanlee96@users.noreply.github.com>
2023-08-31 00:33:09 +0900

    설정 및 dto 패키지 추가

---
+++ src/main/java/com/sparta/post/dto/SignupUserRequestDto.java
@@ -0,0 +1,9 @@
+package com.sparta.post.dto;
+
+import lombok.Getter;
+
+@Getter
+public class SignupUserRequestDto {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/sparta/post/dto/SignupUserReseponseDto.java
@@ -0,0 +1,8 @@
+package com.sparta.post.dto;
+
+public class SignupUserReseponseDto {
+    private Long id;
+    private String username;
+    private String password;
+
+}
